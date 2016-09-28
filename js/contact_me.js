55b5fd935c1473f08647e27c8307bfe648d97892
Djjdekoning <djjdekoning@gmail.com>
2016-09-28 21:16:50 +0200

    test

--- js/contact_me.js
+++ js/contact_me.js
@@ -48,7 +48,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", blijkbaar doet het systeem het niet. Mijn excuses, probeer het later nog eens.");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", blijkbaar doet het systeem het nu niet. Mijn excuses, probeer het later nog eens.");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
