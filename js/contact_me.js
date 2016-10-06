f3dd91562991977135f56b325bcf93a39a1260dd
Djjdekoning <djjdekoning@gmail.com>
2016-10-06 13:42:52 +0200

    contactformulier

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,15 +20,15 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                // url: "././mail/contact_me.php",
                 url:"https://formspree.io/djjdekoning@gmail.com",
-                type: "POST",
+                method: "POST",
                 data: {
                     name: name,
                     phone: phone,
                     email: email,
                     message: message
                 },
+                dataType: "json",
                 cache: false,
                 success: function() {
                     // Success message
@@ -36,10 +36,10 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Bedankt voor u bericht, ik neem snel contact met u op. </strong>");
+                        .append("<strong>Your message has been sent. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
- 
+
                     //clear all fields
                     $('#contactForm').trigger("reset");
                 },
@@ -48,7 +48,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", er is iets mis gegaan. Probeer het later nog eens.");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
