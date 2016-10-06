4d74ed77be7cf23b3f051b55941ca78854019bc1
Djjdekoning <djjdekoning@gmail.com>
2016-10-06 13:47:28 +0200

    Contactformulier.
    - Tekst aangepast voor een verzonden bericht.

--- js/contact_me.js
+++ js/contact_me.js
@@ -36,7 +36,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Bedankt voor u bericht :) </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -48,7 +48,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", er is iets mis gegaan. Probeer het later nog eens.");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
